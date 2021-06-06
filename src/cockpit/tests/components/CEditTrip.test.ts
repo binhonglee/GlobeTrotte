82b5b219d05147bdd5bddd7223c771bf8962a19b
Samuel Lee <samueljklee@gmail.com>
2021-06-06 00:26:21 -0700

    test: add CEditTrip jest test

--- src/cockpit/tests/components/CEditTrip.test.ts
+++ src/cockpit/tests/components/CEditTrip.test.ts
@@ -5,17 +5,32 @@ import CEditTrip from "@/components/CEditTrip.vue";
 import { mockTripObj } from "@/tests/mockData/data";
 
 import { shallowMount } from "@vue/test-utils";
-import test from "ava";
+import { expect } from "@jest/globals";
 
 const mockedTrip = new mockTripObj();
 
-test("renders empty component", (t) => {
-  const trip = mockedTrip.trip;
-  const wrapper = shallowMount(CEditTrip, {
-    props: {
-      trip: trip,
-    },
-    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  } as any);
-  t.regex(wrapper.find(".editLabel").text(), /City:/);
+describe("CEditTrip", () => {
+  it("renders component", () => {
+    const trip = mockedTrip.trip;
+    const wrapper = shallowMount(CEditTrip, {
+      props: {
+        trip,
+      },
+    });
+    const editCity = wrapper.find(".editCity");
+    expect(editCity.exists()).toBeTruthy();
+    expect(editCity.text()).toContain("Cities");
+  });
+
+  it("simulate save trip", () => {
+    const trip = mockedTrip.trip;
+    const wrapper = shallowMount(CEditTrip, {
+      props: {
+        trip,
+      },
+    });
+    const saveEditTrip = wrapper.find(".saveEditTrip");
+    saveEditTrip.trigger("click");
+    expect(saveEditTrip.exists()).toBeTruthy();
+  });
 });
