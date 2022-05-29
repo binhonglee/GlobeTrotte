681eb4bd95865a9560d577c80ce09a09ca675f83
BinHong Lee <binhong@binhong.me>
2022-05-29 00:02:58 -0700

    Fix cli not working as intended

--- src/altimeter/cli.ts
+++ src/altimeter/cli.ts
@@ -1,10 +1,17 @@
 import { readFileSync } from "fs";
+import { exit } from "process";
 import { genRun, AltimeterConfig } from ".";
 
 const args = process.argv.slice(2);
-const config = Object.setPrototypeOf(
+
+if (args.length < 1) {
+  console.error("Please define a config file.");
+  exit(1);
+}
+
+const config = Object.assign(
+  new AltimeterConfig(),
   JSON.parse(readFileSync(args[0], "utf8")),
-  AltimeterConfig.prototype,
 );
 
 genRun(config);
